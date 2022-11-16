92fe8ced140f18461d38330cb0774b06ab469d44
lavinialazzeroni <118049473+lavinialazzeroni@users.noreply.github.com>
2022-11-16 14:41:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main()
+{
+   float a=0, b=0, x=0, y=0, z=0;
+   cout<<"dire valore di a,b,x,y\n" ;
+   cin>>a>>b>>x>>y;
+   if  (x < 0 && y > 0) {
+       z = a*x-b*y;
+   }
+   else if (x >= 0 && y <= 0) {
+       z = a*x*x-b*y;
+   }
+   else { 
+       z = a*x + b*y*y;
+   }
+   cout<<z;
 }
